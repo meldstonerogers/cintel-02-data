2c21d1bfdf5166c815de59018f5cfc810ca4d2f1
Melissa Stone Rogers <168462484+meldstonerogers@users.noreply.github.com>
2024-11-03 13:25:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from shiny.express import input, ui
 from shiny import render
 from shinywidgets import render_plotly
 import palmerpenguins  # This package provides the Palmer Penguins dataset
+from shinyswatch import theme
 
 # Use the built-in function to load the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Melissa's Penguin Practice Data", fillable=True)
-
+ui.page_opts(title="Melissa's Penguin Practice Data", fillable=True, theme=theme.morph)
 
 # Add a Shiny UI sidebar for user interaction
 # Use a with block to add content to the sidebar
 with ui.sidebar(bg="#8fb597"):  
-    ui.h2("Sidebar", style="color:#495569") # Use the ui.h2() function to add a 2nd level header to the sidebar    
+    ui.h2("Sidebar") # Use the ui.h2() function to add a 2nd level header to the sidebar    
     ui.hr(), # Use ui.hr() to add a horizontal rule to the sidebar 
 #ADD ADDITIONAL ATTRIBUTES 
 
@@ -60,12 +60,36 @@ with ui.sidebar(bg="#8fb597"):
     ui.a("Melissa's GitHub", href="https://github.com/meldstonerogers", target="_blank") 
 
 #Main Content 
+#Data Table, showing all data
+#Data Grid, showing all data
+
 with ui.layout_columns():
+    with ui.card(full_screen=True):
+            ui.card_header("Data Table")
+            
+            @render.data_frame  
+            def penguins_table():
+                return render.DataTable(
+                    penguins_df, selection_mode="row", width="400px", height="250px"
+            )  
 
-    @render_plotly
-    def plot1():
-        return px.histogram(px.data.tips(), y="tip")
+    with ui.card(full_screen=True):
+            ui.card_header("Data Grid")
+        
+            @render.data_frame  
+            def penguins_grid():
+                return render.DataGrid(penguins_df) 
 
-    @render_plotly
-    def plot2():
-        return px.histogram(px.data.tips(), y="total_bill")
+#Plotly Histogram, showing all species 
+#Seaborn Histogram, showing all species 
+
+#Plotly Scatterplot, showing all species 
+#with ui.card(full_screen=True):
+
+   # ui.card_header("Plotly Scatterplot: Species")
+
+   # @render_plotly
+   # def plotly_scatterplot():
+        # Create a Plotly scatterplot using Plotly Express
+        # Call px.scatter() function
+        # Pass in six arguments:
